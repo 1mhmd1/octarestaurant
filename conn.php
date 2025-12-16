81a86e24f697470fbba50c37a88b36266777ce65
waledayoub-77 <waledayoub14@gmail.com>
2025-12-16 13:58:16 +0200

    add connection

---
+++ conn.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost";  
+$dbname = "restaurant";
+$username = "root";   
+$password = "";
+$conn = mysqli_connect($host, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
