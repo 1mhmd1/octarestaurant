dd40f513576ca42e6a5ea121a1b1e4abbbbe21b2
waledayoub-77 <waledayoub14@gmail.com>
2025-12-22 13:16:36 +0200

    feat: implement category handling during restaurant addition

--- createRest.php
+++ createRest.php
@@ -31,6 +31,26 @@ if ($result === false ) {
     exit;
 }
 $rest_id = $conn->insert_id;
+$stmt_category= $conn->prepare("SELECT rest_cat_id FROM rest_category WHERE cat_name = ?");
+$stmt_insert = $conn->prepare("INSERT INTO rest_rest_category (rest_id , rest_cat_id )  VALUES (?, ?)");
+foreach($categories as $cat_name){
+    $stmt_category->bind_param('s', $cat_name);
+    $stmt_category->execute();
+    $result = $stmt_category->get_result();
+    if($row = $result->fetch_assoc()){
+    $rest_cat_id = $row['rest_cat_id'];
+    $stmt_insert->bind_param('ii', $rest_id, $rest_cat_id);
+    $stmt_insert->execute();
+    }
+    else{
+    $stmt_new = $conn->prepare("INSERT INTO rest_category (cat_name) VALUES (?)");
+    $stmt_new->bind_param('s', $cat_name);
+    $stmt_new->execute();
+    $rest_cat_id = $stmt_new->insert_id;
+    $stmt_insert->bind_param('ii', $rest_id, $rest_cat_id);
+    $stmt_insert->execute();
+    }
+}
 
 echo json_encode(["status"=>"success","message"=>"Restaurant added successfully"]);
 
