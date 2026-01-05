d0a1c16bb2d541405c8db4af751e7b3164c17038
MhmdSalim <151769456+1mhmd1@users.noreply.github.com>
2026-01-05 14:40:04 +0200

    Implement CORS support in conn.php
    Add CORS headers and handle preflight requests.

--- connection/conn.php
+++ connection/conn.php
@@ -2,6 +2,9 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
 $host = "centerbeam.proxy.rlwy.net";
 $port = 59934;
 $user = "root";
