614054e2cb60866b48bd01f2f181d5d85789bf44
MhmdSalim <151769456+1mhmd1@users.noreply.github.com>
2026-01-05 14:44:43 +0200

    Remove OPTIONS request handling in conn.php
    Removed OPTIONS request handling from connection script.

--- connection/conn.php
+++ connection/conn.php
@@ -2,9 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
+
 $host = "centerbeam.proxy.rlwy.net";
 $port = 59934;
 $user = "root";
